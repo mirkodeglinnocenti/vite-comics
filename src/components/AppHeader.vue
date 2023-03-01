bc0ea2b70657c903589544324aca517f55e4e1da
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-03-01 15:58:19 +0100

    FEAT favicon and title added and create AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        ciao
+    </div>
+
+</template>
+
+
+<script>
+    export default {
+
+    }
+</script>
+
+
+<style lang="scss" scoped>
+@use '../style/partials/reset' as *;
+
+</style>
