c38aa6cf18e8d4d6c6a443894a04a9020a1417f9
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-03-02 17:14:56 +0100

    FEAT card component added

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,8 +3,21 @@
     <main>
         <section>
             <div class="container">
-                <p class="text-example"> --> Content goes Here &lt;-- </p>
+                <p class="text-example"> Scrivi qua </p>
+                
             </div>
+
+            <section class="poster-section">
+                <div class="container">
+                    <div class="poster-row">
+
+                        <ComicsCard v-for="(posterItem, i) in posters" :key="i" :poster="posterItem"/>
+
+                    </div>
+                </div>
+            </section>
+            
+            
         </section>
     </main>
 
@@ -12,8 +25,91 @@
 
 
 <script>
-export default {
+import ComicsCard from './ComicsCard.vue';
 
+export default {
+    components: {
+        ComicsCard,
+    },
+    data () {
+        return{
+            posters: [
+                {
+                    "thumb": "/img/superman.png",
+                    "price": "$19.99",
+                    "series": "Action Comics",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "/img/american-vampire.png",
+                    "price": "$3.99",
+                    "series": "American Vampire 1976",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "/img/acquaman.png",
+                    "price": "$16.99",
+                    "series": "Aquaman",
+                    "type": "graphic novel"
+                },
+                {
+                    "thumb": "/img/batgirl.png",
+                    "price": "$2.99",
+                    "series": "Batgirl",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "/img/batman.png",
+                    "price": "$3.99",
+                    "series": "Batman",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "/img/batman.png",
+                    "price": "$2.99",
+                    "series": "Batman Beyond",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "/img/superman.png",
+                    "price": "$3.99",
+                    "series": "Batman/Superman",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "/img/superman.png",
+                    "price": "$4.99",
+                    "series": "Batman/Superman Annual",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "/img/superman.png",
+                    "price": "$5.99",
+                    "series": "Batman: The Joker War Zone",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "/img/superman.png",
+                    "price": "$6.99",
+                    "series": "Batman: Three Jokers",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "/img/superman.png",
+                    "price": "$4.99",
+                    "series": "Batman: White Knight Presents: Harley Quinn",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "/img/superman.png",
+                    "price": "$16.99",
+                    "series": "Catwoman",
+                    "type": "graphic novel"
+                }
+            ]
+
+        }
+    }
 }
 </script>
 
@@ -22,7 +118,6 @@ export default {
 @use '../style/partials/reset' as *;
 
 section {
-    height: 132px;
     background-color: #1C1C1C;
     color: white;
 
@@ -36,6 +131,19 @@ section {
     .text-example{
         font-size: 36px;
     }
+}
+
+.poster-section {
+
+    .poster-row{
+        display: flex;
+        flex-wrap: wrap;
+        gap: 20px;
+    }
+    .poster-col{
+        width: calc((100%/6) - (100px / 6));
+    }
+
 }
 
 </style>
---
+++ src/components/ComicsCard.vue
@@ -0,0 +1,44 @@
+<template>
+
+
+    <div class="poster-col">
+        <img :src="poster.thumb" alt="">
+        <p>{{poster.series}}</p>
+    </div>
+
+
+</template>
+
+<script>
+export default {
+    props:{
+        poster: {
+            type: Object,
+            required: true
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../style/general.scss' as *;
+
+
+.poster-col{
+    width: calc((100%/6) - (100px / 6));
+
+    img{
+        width: 100%;
+        display: block;
+        margin-bottom: 15px;
+    }
+
+    p{  
+        font-size: 14px;
+        text-transform: uppercase;
+    }
+}
+
+
+</style>
